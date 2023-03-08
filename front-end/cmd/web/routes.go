7906a0a00aeee48aa2b695f0302376afb4bd7899
Philip-21 <philipuzomaobiora@gmail.com>
2023-03-08 10:23:25 +0100

    New Changes made to templates to handle inputs onclicks

--- front-end/cmd/web/routes.go
+++ front-end/cmd/web/routes.go
@@ -16,23 +16,5 @@ func (app *Config) routes() http.Handler {
 		app.renderHome(w, "test.page.go.html")
 	})
 
-	mux.HandleFunc("/auth", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		//w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
-		app.renderAuth(w, "auth.page.go.html")
-	})
-
-	mux.HandleFunc("/signup", func(w http.ResponseWriter, r *http.Request) {
-
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		//w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
-		app.renderSign(w, "signup.page.go.html")
-
-	})
-
 	return mux
 }
